19bda3304652a2fb1fdd244358222673900f57d9
Bruno Montezano <brunodoyt@gmail.com>
2023-05-14 10:36:38 -0300

    adicionar alunos no alunos.csv e adicionar condicional para pular compilação de certificados já existentes

--- certificados/gerar_certificado.R
+++ certificados/gerar_certificado.R
@@ -13,32 +13,36 @@
 gerar_certificado <-
   function(modelo,
            nome_aluno,
-           nome_evento,
-           data_evento,
-           local_evento,
            saida_pdf,
+           caminho_pdf,
            diretorio_rmd) {
-    cat("\n Iniciando a compilação:", saida_pdf, "\n")
-    
-    # Criar um arquivo qmd temporário com aluno e informações do evento
-    modelo_certificado <- readr::read_file(modelo)
-    
-    # Modificar NOMEALUNO pelo nome do aluno para o modelo
-    # temporário de cada iteração
-    modelo_tmp <- modelo_certificado |> 
-      stringr::str_replace("NOMEALUNO", nome_aluno)
-    
-    # O diretorio_rmd tem que ser definido para o
-    # rmarkdown::render() saber onde salvar o arquivo temporário
-    arquivo_rmd <- tempfile(tmpdir = diretorio_rmd, fileext = ".Rmd")
-    readr::write_file(modelo_tmp, arquivo_rmd)
-    
-    # Criar os certificados usando R Markdown
-    rmarkdown::render(arquivo_rmd,
-                      output_file = saida_pdf,
-                      quiet = TRUE)
-    
-    # Arquivo .Rmd temporário pode ser excluído após cada iteração
-    file.remove(arquivo_rmd)
-    cat("\n Finalizado:", saida_pdf, "\n")
+    if (file.exists(caminho_pdf)) {
+      cat("\n Pulando arquivo:", saida_pdf, "\n")
+    }
+    else {
+      cat("\n Iniciando a compilação:", saida_pdf, "\n")
+      
+      # Criar um arquivo qmd temporário com aluno e informações do evento
+      modelo_certificado <- readr::read_file(modelo)
+      
+      # Modificar NOMEALUNO pelo nome do aluno para o modelo
+      # temporário de cada iteração
+      modelo_tmp <- modelo_certificado |>
+        stringr::str_replace("NOMEALUNO", nome_aluno)
+      
+      # O diretorio_rmd tem que ser definido para o
+      # rmarkdown::render() saber onde salvar o arquivo temporário
+      arquivo_rmd <-
+        tempfile(tmpdir = diretorio_rmd, fileext = ".Rmd")
+      readr::write_file(modelo_tmp, arquivo_rmd)
+      
+      # Criar os certificados usando R Markdown
+      rmarkdown::render(arquivo_rmd,
+                        output_file = saida_pdf,
+                        quiet = TRUE)
+      
+      # Arquivo .Rmd temporário pode ser excluído após cada iteração
+      file.remove(arquivo_rmd)
+      cat("\n Finalizado:", saida_pdf, "\n")
+    }
   }
--- certificados/gerar_pdfs.R
+++ certificados/gerar_pdfs.R
@@ -7,20 +7,33 @@ source("certificados/gerar_certificado.R")
 # Carregar arquivo com informações dos alunos
 alunos <- readr::read_csv("certificados/alunos.csv") |>
   # Criar variável do caminho do arquivo em PDF para o certificado
-  dplyr::mutate(arquivo_pdf = stringr::str_c(
-    "../PDF/",
-    dplyr::row_number(),
-    "_",
-    stringr::str_replace_all(nome, stringr::fixed(" "), "_"),
-    ".pdf"
-  ))
+  dplyr::mutate(
+    arquivo_pdf = stringr::str_c(
+      "../PDF/",
+      dplyr::row_number(),
+      "_",
+      stringr::str_replace_all(nome, stringr::fixed(" "), "_"),
+      ".pdf"
+    ),
+    caminho_pdf = stringr::str_c(
+      "certificados/PDF/",
+      dplyr::row_number(),
+      "_",
+      stringr::str_replace_all(nome, stringr::fixed(" "), "_"),
+      ".pdf"
+    )
+  )
 
 # Loop para gerar certificado com nome de todos os alunos
 for (i in seq_len(nrow(alunos))) {
   with(
     alunos,
-    gerar_certificado(modelo = "certificados/modelo_certificado.Rmd",
-                nome[i], evento[i], data[i], local[i], arquivo_pdf[i], "certificados/Rmd")
+    gerar_certificado(
+      modelo = "certificados/modelo_certificado.Rmd",
+      nome_aluno = nome[i],
+      saida_pdf = arquivo_pdf[i],
+      caminho_pdf = caminho_pdf[i],
+      diretorio_rmd = "certificados/Rmd"
+    )
   )
 }
-
